cfcb0e511aad8b27337bde492b5604bddce9342c
John <hjohnboga@gmail.com>
2021-06-21 01:09:49 +0300

    updated inertia and inertia-vue3, tested the new inertia-head template

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <template>
+  <inertia-head title="Dashboard" />
   <breeze-authenticated-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
