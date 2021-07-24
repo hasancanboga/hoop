1a5209feed82b5f35a89f0927f85cacf0ed826ce
John <hjohnboga@gmail.com>
2021-07-24 12:23:08 +0300

    images, videos, stock_images, challenges, stunts models and migrations.

---
+++ app/Models/Stunt.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Stunt extends Model
+{
+    use HasFactory;
+
+    public function challenge(): BelongsTo
+    {
+        return $this->belongsTo(Challenge::class);
+    }
+}
---
+++ database/migrations/2021_07_24_130754_create_challenges_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->integer('min_age');
+            $table->integer('max_age');
+            $table->integer('max_participants')->nullable();
+            $table->string('reward');
+            $table->foreignId('stock_image_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->foreignId('video_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('set null');;
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
